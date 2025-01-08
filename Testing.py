2da4b08adef184180bb5ce773800d97736c9f5bc
Simon <s.carbone24@studenti.unisa.it>
2025-01-08 20:15:29 +0100

    Modifiche varie al codice

--- Testing.py
+++ Testing.py
@@ -65,78 +65,135 @@ def generate_population(pop_size, num_cells, total_resources, min_resources):
 
 
 # Parametri iniziali
-POP_SIZE = 20
+POP_SIZE = 40
 NUM_CELLS = 25
 TOTAL_RESOURCES = 2000
 MIN_RESOURCES = 20
 TOURNAMENT_SIZE = 5
 CROSSOVER_POINTS = 4
+MAX_GENERATIONS = 10
+TARGET_FITNESS = 28  # Soglia di fitness target
 
 # Parametri di mutazione
 mutation_rate = 0.10
 fitness_threshold = 0.02  # 2% miglioramento minimo
-previous_fitness = 0.0  # Fitness media iniziale
+previous_fitness = 1  # Fitness media iniziale
 
 # Generazione della popolazione iniziale
 population = generate_population(POP_SIZE, NUM_CELLS, TOTAL_RESOURCES, MIN_RESOURCES)
 
-# Stampa alcuni risultati di esempio per vedere la distribuzione delle risorse
-for i, individual in enumerate(population):
-    print(f"Individuo {i}: {individual[:25]}... (total resources: {sum(individual)- individual[0]})")
-
-# Valutazione della fitness degli individui
-lambda_value = 1.0  # Puoi scegliere il valore di lambda_value secondo le tue esigenze
-fitness_values = [fitness_function(individual, lambda_value, index)for index, individual in enumerate(population)]
-
-# Selezione con il K-Way Tournament
-selected_individuals = k_way_tournament_min(population, fitness_values, TOURNAMENT_SIZE)
+# Ciclo evolutivo
+for generation in range(MAX_GENERATIONS):
+    print(f"Generazione {generation}")
 
 
-# Crossover tra i primi due individui selezionati
-parent1 = selected_individuals[0]
-parent2 = selected_individuals[1]
-fitness_values_parent = [fitness_function_parent(parent1, lambda_value), fitness_function_parent(parent2, lambda_value)]
+    # Stampa alcuni risultati di esempio per vedere la distribuzione delle risorse
+    for i, individual in enumerate(population):
+        print(f"Individuo {i}: {individual[:25]}... (total resources: {sum(individual)- individual[0]})")
 
+    # Valutazione della fitness degli individui
+    lambda_value = 1.0  # Puoi scegliere il valore di lambda_value secondo le tue esigenze
+    fitness_values = [fitness_function(individual, lambda_value, index)for index, individual in enumerate(population)]
 
+        # Stampa migliori fitness
+    min_fitness = min(fitness_values)
+    print(f"\n  Fitness migliore: {min_fitness}")
 
-# Converti i figli in liste di interi normali per la stampa
-child1, child2 = k_point_crossover(parent1, parent2, CROSSOVER_POINTS)
-child1 = [int(x) for x in child1]
-child2 = [int(x) for x in child2]
+    if min_fitness <= TARGET_FITNESS:
+        print("\n>>> Soluzione ottimale trovata!")
+        break
 
+    # Selezione con il K-Way Tournament
+    selected_individuals = k_way_tournament_min(population, fitness_values, TOURNAMENT_SIZE)
 
-# Calcolo della fitness corrente prima della mutazione
-current_fitness = (fitness_function(child1, lambda_value, 0) + fitness_function(child2, lambda_value, 1)) / 2
+    # Crossover tra i primi due individui selezionati
+    parent1 = selected_individuals[0]
+    parent2 = selected_individuals[1]
+    parent3 = selected_individuals[2]
+    parent4 = selected_individuals[3]
+    parent5 = selected_individuals[4]
+    parent6 = selected_individuals[5]
+    fitness_values_parent = [fitness_function_parent(parent1, lambda_value), fitness_function_parent(parent2, lambda_value), fitness_function_parent(parent3, lambda_value), fitness_function_parent(parent4, lambda_value), fitness_function_parent(parent5, lambda_value), fitness_function_parent(parent6, lambda_value)]
 
-# Calcolo del cambiamento di fitness e aggiornamento del tasso di mutazione
-fitness_change = calculate_fitness_change(previous_fitness, current_fitness)
-mutation_rate = adaptive_mutation(mutation_rate, fitness_change, fitness_threshold)
-print(f"\nFitness Change: {fitness_change:.4f}, Updated Mutation Rate: {mutation_rate:.4f}")
+    # Converti i figli in liste di interi normali per la stampa
+    child1, child2 = k_point_crossover(parent1, parent2, CROSSOVER_POINTS)
+    child3, child4 = k_point_crossover(parent3, parent4, CROSSOVER_POINTS)
+    child5, child6 = k_point_crossover(parent5, parent6, CROSSOVER_POINTS)
+    child1 = [int(x) for x in child1]
+    child2 = [int(x) for x in child2]
+    child3 = [int(x) for x in child3]
+    child4 = [int(x) for x in child4]
+    child5 = [int(x) for x in child5]
+    child6 = [int(x) for x in child6]
 
+    # Output per verificare
+    print("\nGenitore 1 scelto dal K-Way Tournament:", parent1)
+    print("\nGenitore 2 scelto dal K-Way Tournament:", parent2)
+    print("\nGenitore 3 scelto dal K-Way Tournament:", parent3)
+    print("\nGenitore 4 scelto dal K-Way Tournament:", parent4)
+    print("\nGenitore 5 scelto dal K-Way Tournament:", parent5)
+    print("\nGenitore 6 scelto dal K-Way Tournament:", parent6)
+    print(f"\nFitness Genitore 1: {fitness_values_parent[0]}")
+    print(f"Fitness Genitore 2: {fitness_values_parent[1]}")
+    print(f"Fitness Genitore 3: {fitness_values_parent[2]}")
+    print(f"Fitness Genitore 4: {fitness_values_parent[3]}")
+    print(f"Fitness Genitore 5: {fitness_values_parent[4]}")
+    print(f"Fitness Genitore 6: {fitness_values_parent[5]}")
 
-# Applica mutazione ai figli
-childmutate1 = mutate(child1, mutation_rate)
-childmutate2 = mutate(child2, mutation_rate)
+    # Conserva i valori di fitness dei figli
+    print(f"\nFiglio 1: {child1} ")
+    print(f"\nFiglio 2: {child2} ")
+    print(f"\nFiglio 3: {child3} ")
+    print(f"\nFiglio 4: {child4} ")
+    print(f"\nFiglio 5: {child5} ")
+    print(f"\nFiglio 6: {child6} \n")
+    fitness_values_children = [fitness_function_parent(child1, lambda_value), fitness_function_parent(child2, lambda_value), fitness_function_parent(child3, lambda_value), fitness_function_parent(child4, lambda_value), fitness_function_parent(child5, lambda_value), fitness_function_parent(child6, lambda_value)]
+    print(f"\nFitness Figlio 1: {fitness_values_children[0]}")
+    print(f"Fitness Figlio 2: {fitness_values_children[1]}")
+    print(f"Fitness Figlio 3: {fitness_values_children[2]}")
+    print(f"Fitness Figlio 4: {fitness_values_children[3]}")
+    print(f"Fitness Figlio 5: {fitness_values_children[4]}")
+    print(f"Fitness Figlio 6: {fitness_values_children[5]}")
 
-# Output per verificare
-print("\nGenitore 1 scelto dal K-Way Tournament:", parent1)
-print("\nGenitore 2 scelto dal K-Way Tournament:", parent2)
-print(f"Fitness Genitore 1: {fitness_values_parent[0]}")
-print(f"Fitness Genitore 2: {fitness_values_parent[1]}")
+    # Calcolo della fitness corrente prima della mutazione
+    current_fitness = (fitness_function_parent(child1, lambda_value) + fitness_function_parent(child2, lambda_value)+fitness_function_parent(child3, lambda_value)+fitness_function_parent(child4, lambda_value)+fitness_function_parent(child5, lambda_value)+fitness_function_parent(child6, lambda_value)) / 6
 
+    # Applica mutazione ai figli
+    childmutate1 = mutate(child1, mutation_rate)
+    childmutate2 = mutate(child2, mutation_rate)
+    childmutate3 = mutate(child3, mutation_rate)
+    childmutate4 = mutate(child4, mutation_rate)
+    childmutate5 = mutate(child5, mutation_rate)
+    childmutate6 = mutate(child6, mutation_rate)
 
-# Conserva i valori di fitness dei figli
-print(f"\nFiglio 1: {child1} ")
-print(f"\nFiglio 2: {child2} ")
-fitness_values_children = [fitness_function(child1, lambda_value, 0), fitness_function(child2, lambda_value, 1)]
-(fitness_values_children[0])
-(fitness_values_children[1])
+    # Calcolo dei valori di fitness per i figli mutati
+    fitness_values_mutate = [
+    fitness_function_parent(childmutate1, lambda_value),
+    fitness_function_parent(childmutate2, lambda_value),
+    fitness_function_parent(childmutate3, lambda_value),
+    fitness_function_parent(childmutate4, lambda_value),
+    fitness_function_parent(childmutate5, lambda_value),
+    fitness_function_parent(childmutate6, lambda_value)
+]
 
-# Conserva i valori di fitness dei figli
-print(f"\nFiglio 1 (mutato): {childmutate1}")
-print(f"\nFiglio 2 (mutato): {childmutate2}")
-fitness_values_children = [fitness_function(childmutate1, lambda_value, 0), fitness_function(childmutate2, lambda_value, 1)]
+    # Stampa dei valori di fitness per i figli mutati
+    print(f"\nFiglio 1 (Mutato): {childmutate1}")
+    print(f"\nFiglio 2 (Mutato): {childmutate2}")
+    print(f"\nFiglio 3 (Mutato): {childmutate3}")
+    print(f"\nFiglio 4 (Mutato): {childmutate4}")
+    print(f"\nFiglio 5 (Mutato): {childmutate5}")
+    print(f"\nFiglio 6 (Mutato): {childmutate6}")
+    print(f"\nFitness Figlio 1 (Mutato): {fitness_values_mutate[0]}")
+    print(f"Fitness Figlio 2 (Mutato): {fitness_values_mutate[1]}")
+    print(f"Fitness Figlio 3 (Mutato): {fitness_values_mutate[2]}")
+    print(f"Fitness Figlio 4 (Mutato): {fitness_values_mutate[3]}")
+    print(f"Fitness Figlio 5 (Mutato): {fitness_values_mutate[4]}")
+    print(f"Fitness Figlio 6 (Mutato): {fitness_values_mutate[5]}")
 
+    # Calcolo del cambiamento di fitness e aggiornamento del tasso di mutazione
+    fitness_change = calculate_fitness_change(previous_fitness, current_fitness)
+    mutation_rate = adaptive_mutation(mutation_rate, fitness_change, fitness_threshold)
+    print(f"  Fitness Change: {fitness_change:.4f}, Mutation Rate: {mutation_rate:.4f}")
+    previous_fitness = current_fitness  # Aggiorna la fitness precedente
 
-
-
+print("\n>>> Evoluzione terminata.")
